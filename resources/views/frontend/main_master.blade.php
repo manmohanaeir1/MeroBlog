b07f8e89bd13f872e58b676bd5c9865901a15f26
Manmohan Aeir <manmohanaeir001110@gmail.com>
2023-10-04 09:48:35 +0545

    frontend setp

---
+++ resources/views/frontend/main_master.blade.php
@@ -0,0 +1,36 @@
+<!doctype html>
+<html class="no-js" lang="en">
+
+@include('frontend.body.head')
+
+    <body>
+
+        <!-- preloader-start -->
+        <div id="preloader">
+            <div class="rasalina-spin-box"></div>
+        </div>
+        <!-- preloader-end -->
+
+		<!-- Scroll-top -->
+        <button class="scroll-top scroll-to-target" data-target="html">
+            <i class="fas fa-angle-up"></i>
+        </button>
+        <!-- Scroll-top-end-->
+
+        <!-- header-area -->
+       @include('frontend.body.header')
+        <!-- header-area-end -->
+
+        <!-- main-area -->
+        <main>
+            @yield('content')
+        </main>
+        <!-- main-area-end -->
+
+
+        @include('frontend.body.footer')
+       
+
+		@include('frontend.body.script')
+    </body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ use App\Http\Controllers\DashboardController;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('frontend.index');
 });
 
 Route::get('/dashboard', function () {
