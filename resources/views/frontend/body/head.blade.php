f83075e31c710517dad65e0b382c075e8a54fc32
Manmohan Aeir <manmohanaeir001110@gmail.com>
2023-10-06 16:48:48 +0545

    Integrated  Image intervention Package

--- resources/views/frontend/body/head.blade.php
+++ resources/views/frontend/body/head.blade.php
@@ -1,11 +1,11 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
-    <title>Rasalina - Personal Portfolio HTML Template</title>
+    <title>MeroBlog - Personal Portfolio HTML Template</title>
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <link rel="shortcut icon" type="image/x-icon" href="{{asset('frontend/assets/images/logo.png')}}">
+    <link rel="shortcut icon" type="image/x-icon" href="{{asset('logo/logo.png')}}">
     <!-- Place favicon.ico in the root directory -->
 
     <!-- CSS here -->
